2ab9da62c5442d497b726199a422da4f66a95feb
Zicka67 <art.book@hotmail.fr>
2023-04-12 16:56:24 +0200

    On ajoute le managerRegistry et la classe Emplyer pour avoir accès aux infos dans la function

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -2,18 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Employe;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EmployeController extends AbstractController
 {
     // Ici on fait le lien entre la méthode et l'URL du navigateur (de la page qu'on veut afficher)
     #[Route('/employe', name: 'app_employe')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        // Récupérer toutes les entreprises de la DB (une collection d'objet entreprise ici) en les classants par ordre alphabétique via 
+        // @method Employe[]  findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null) qui est une méthode de base dans Repository
+        // ICI $doctrine->getRepository(Employe::class)->findBy(["ville" => "STRASBOURG"], ["nom" => "ASC"]); = SELECT *, FROM employe, WHERE ville = STRASBOURG, ORDER BY ASC
+        $employes = $doctrine->getRepository(Employe::class)->findBy(["ville" => "STRASBOURG"], ["nom" => "ASC"]);
         return $this->render('employe/index.html.twig', [
-            'controller_name' => 'EmployeController',
+            'employes' => $employes
         ]);
     }
 }
