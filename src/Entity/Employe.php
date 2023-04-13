8283f36f46aed41b6c6da97c80db968a398e0754
Zicka67 <art.book@hotmail.fr>
2023-04-13 11:27:14 +0200

    Ajout d'une function pour calculer l'âge

--- src/Entity/Employe.php
+++ src/Entity/Employe.php
@@ -112,6 +112,13 @@ class Employe
         return $this;
     }
 
+    // Calculer l'age de la personne
+    public function getAge () {
+        $now = new \DateTime();
+        $interval = date_diff($this->dateNaissance, $now);
+        return $interval->format("%Y");
+    }
+
     public function __toString()
     {
         // return $this->prenom. " " .$this->nom. " " . $this->ville. "(" .$this->getEntreprise(). ")";
