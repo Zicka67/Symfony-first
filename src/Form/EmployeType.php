c8975585e9fd7db9b13c7bb1b1f74443947ed2be
Zicka67 <art.book@hotmail.fr>
2023-04-13 16:52:12 +0200

    Modification pour le form de l'employe

--- src/Form/EmployeType.php
+++ src/Form/EmployeType.php
@@ -6,18 +6,38 @@ use App\Entity\Employe;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class EmployeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('dateNaissance')
-            ->add('dateEmbauche')
-            ->add('ville')
-            ->add('entreprise')
+            ->add('nom', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('prenom', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('dateNaissance', DateType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('dateEmbauche', DateType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('ville', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('entreprise', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            //On rajoute TextType pour signaler qu'on veut un champ de text et on ajoute la class (clic droit -> ajouter class -> Component)
+            ->add('submit', SubmitType::class, [
+                //Pour ajouter un attr qui sera une classe
+                'attr' => ['class' => 'btn btn-primary']
+            ])
         ;
     }
 
